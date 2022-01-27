2232419213c94484913e57f308bee48678458ad0
TsaiTing Wang <tsaiting.wang@fleetcor.com>
2022-01-27 15:28:33 -0500

    incremental checkins

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,15 @@
 import { checkForInput } from './js/nameChecker'
 import {checkForURL} from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import {appendScores, getScores, handleSubmit} from './js/formHandler'
 import { handleSubmit2 } from './js/formHandler'
 import { onBlur } from './js/formHandler'
 import { onFocus } from './js/formHandler'
 import {postData} from "./js/formPost";
 import {updateUI} from "./js/formPost";
 import {updateUI2} from "./js/formPost";
+import {createScoreboardTable} from './js/formHandler'
+import {appendScores} from './js/formHandler'
+import {getScores} from './js/formHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -23,7 +26,10 @@ export {
     onFocus,
     postData,
     updateUI,
-    updateUI2
+    updateUI2,
+    createScoreboardTable,
+    appendScores,
+    getScores
 }
 //console.log(checkForInput);
 
